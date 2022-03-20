cfa017aea28a438fcc06b4b1231fd45dae91b21c
sindhueswaran <54274176+sindhueswaran@users.noreply.github.com>
2022-03-20 19:34:38 +0530

    Update team.js

--- src/components/team.js
+++ src/components/team.js
@@ -6,7 +6,7 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import { Container} from 'react-bootstrap'  
 
 const TEAMSECTION = {
-  title: 'Our team',
+  title: 'About us',
   text: 'Barbara and Sindhu’s collaboration and friendship started spontaneously in 2018 when they met in Rishikesh.',
 } 
 
